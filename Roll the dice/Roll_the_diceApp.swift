abc10d13eb1156a6424fdddcccc04508fecaf47b
Victor <victor>
2021-11-08 10:35:15 -0600

    Initial Commit

---
+++ Roll the dice/Roll_the_diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_diceApp.swift
+//  Roll the dice
+//
+//  Created by Student on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
